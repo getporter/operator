165bd46c0bdcd1bbf464a2b545cd74ac295bba96
Steven Gettys <s.gettys@f5.com>
2022-08-18 11:35:03 -0700

    added integration test bundle that uses state in porter.yaml
    Signed-off-by: Steven Gettys <s.gettys@f5.com>

--- tests/integration/credset_test.go
+++ tests/integration/credset_test.go
@@ -282,8 +282,8 @@ func NewTestInstallation(iName string) *porterv1.Installation {
 			SchemaVersion: string(storage.InstallationSchemaVersion),
 			Name:          iName,
 			Bundle: porterv1.OCIReferenceParts{
-				Repository: "ghcr.io/bdegeeter/porter-test-me",
-				Version:    "0.3.0",
+				Repository: "ghcr.io/sgettys/porter-test-me",
+				Version:    "0.6.0",
 			},
 		},
 	}
