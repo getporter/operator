a4f4c8c9f8a5e24b3fee8fe51d306665ebe5edf4
Sarah Christoff <28318173+schristoff@users.noreply.github.com>
2023-08-09 16:10:57 -0600

    I am not sure if this is even right
    Signed-off-by: Sarah Christoff <28318173+schristoff@users.noreply.github.com>

--- controllers/agentaction_controller.go
+++ controllers/agentaction_controller.go
@@ -254,6 +254,16 @@ func (r *AgentActionReconciler) createAgentVolume(ctx context.Context, log logr.
 			GenerateName: action.Name + "-",
 			Namespace:    action.Namespace,
 			Labels:       labels,
+			OwnerReferences: []metav1.OwnerReference{
+				{
+					APIVersion:         action.APIVersion,
+					Kind:               action.Kind,
+					Name:               action.Name,
+					UID:                action.UID,
+					Controller:         pointer.Bool(true),
+					BlockOwnerDeletion: pointer.Bool(true),
+				},
+			},
 		},
 		Spec: corev1.PersistentVolumeClaimSpec{
 			AccessModes: []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce},
@@ -302,6 +312,16 @@ func (r *AgentActionReconciler) createConfigSecret(ctx context.Context, log logr
 			GenerateName: action.Name + "-",
 			Namespace:    action.Namespace,
 			Labels:       labels,
+			OwnerReferences: []metav1.OwnerReference{
+				{
+					APIVersion:         action.APIVersion,
+					Kind:               action.Kind,
+					Name:               action.Name,
+					UID:                action.UID,
+					Controller:         pointer.Bool(true),
+					BlockOwnerDeletion: pointer.Bool(true),
+				},
+			},
 		},
 		Type:      corev1.SecretTypeOpaque,
 		Immutable: pointer.Bool(true),
@@ -338,6 +358,16 @@ func (r *AgentActionReconciler) createWorkdirSecret(ctx context.Context, log log
 			GenerateName: action.Name + "-",
 			Namespace:    action.Namespace,
 			Labels:       labels,
+			OwnerReferences: []metav1.OwnerReference{
+				{
+					APIVersion:         action.APIVersion,
+					Kind:               action.Kind,
+					Name:               action.Name,
+					UID:                action.UID,
+					Controller:         pointer.Bool(true),
+					BlockOwnerDeletion: pointer.Bool(true),
+				},
+			},
 		},
 		Type:      corev1.SecretTypeOpaque,
 		Immutable: pointer.Bool(true),
