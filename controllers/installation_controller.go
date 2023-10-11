fd9ec00c93224b94b51d1aa934a8bc7e7fbb0981
Troy Connor <troy0820@users.noreply.github.com>
2023-10-11 10:11:57 -0400

    add Recorder to installation reconciler
    Signed-off-by: Troy Connor <troy0820@users.noreply.github.com>

--- controllers/installation_controller.go
+++ controllers/installation_controller.go
@@ -16,6 +16,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/tools/record"
 	"k8s.io/utils/ptr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
@@ -32,6 +33,7 @@ type InstallationReconciler struct {
 	client.Client
 	Log              logr.Logger
 	PorterGRPCClient PorterClient
+	Recorder         record.EventRecorder
 	Scheme           *runtime.Scheme
 }
 
